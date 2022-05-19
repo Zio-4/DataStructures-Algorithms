e80635b074acde2cd926914ff15dfcb517bcf9c5
Philip Ziolkowski <80180474+Zio-4@users.noreply.github.com>
2022-05-19 15:56:07 -0700

    Time: 65 ms (84.66%), Space: 42.7 MB (41.60%) - LeetHub

---
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minAddToMakeValid = function(S) {
+  const stack = []
+  for(let s of S){
+    if(s === ')' && stack[stack.length-1] === '(')
+      stack.pop()
+    else
+      stack.push(s)
+  }
+  return stack.length
+};
+
+// ()))((
+    
+//  ))(())
