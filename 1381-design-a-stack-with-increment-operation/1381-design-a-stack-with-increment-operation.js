85699326db6dd8b5346b83e9041667d64f9ac727
Philip Ziolkowski <80180474+Zio-4@users.noreply.github.com>
2022-05-20 17:18:40 -0700

    Time: 240 ms (15.00%), Space: 50.6 MB (26.00%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.js
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.js
@@ -1,9 +1,57 @@
+// /**
+//  * @param {number} maxSize
+//  */
+// var CustomStack = function(maxSize) {
+//     this.stack = new Array(maxSize)
+//     this.maxSize = maxSize
+// };
+
+// /** 
+//  * @param {number} x
+//  * @return {void}
+//  */
+// CustomStack.prototype.push = function(x) {
+//     if (this.stack.length < this.maxSize) this.stack.push(x)
+// };
+
+// /**
+//  * @return {number}
+//  */
+// CustomStack.prototype.pop = function() {
+//     if (this.stack.length > 0) {
+//         return this.stack.pop()
+//     }
+//     else {
+//         return -1
+//     }
+// };
+
+// /** 
+//  * @param {number} k 
+//  * @param {number} val
+//  * @return {void}
+//  */
+// CustomStack.prototype.increment = function(k, val) {
+//     // If there are less than k elements in the stack
+//     if (k > this.stack.length) {
+//         this.stack = this.stack.map(ele => ele + val)
+//     } else {
+//         this.stack = this.stack.map((ele, idx) => {
+//             if (idx < k){
+//                return ele + val
+//             } else return ele
+//         }) 
+//     }
+//     console.log(this.stack)
+// };
+
 /**
  * @param {number} maxSize
  */
 var CustomStack = function(maxSize) {
-    this.stack = []
-    this.maxSize = maxSize
+    this.stack = [];
+    this.inc = new Array(maxSize).fill(0);
+    this.size = maxSize;
 };
 
 /** 
@@ -11,18 +59,23 @@ var CustomStack = function(maxSize) {
  * @return {void}
  */
 CustomStack.prototype.push = function(x) {
-    if (this.stack.length < this.maxSize) this.stack.push(x)
+    if (this.stack.length < this.size) {
+        this.stack.push(x);
+    }
 };
 
 /**
  * @return {number}
  */
 CustomStack.prototype.pop = function() {
-    if (this.stack.length > 0) {
-        return this.stack.pop()
-    }
-    else {
-        return -1
+    if (this.stack.length) {
+        const idx = this.stack.length - 1;
+        const inc = this.inc[idx];
+        if (idx - 1 >= 0) this.inc[idx - 1] += inc;
+        this.inc[idx] = 0;
+        return this.stack.pop() + inc;
+    } else {
+        return -1;
     }
 };
 
@@ -32,17 +85,8 @@ CustomStack.prototype.pop = function() {
  * @return {void}
  */
 CustomStack.prototype.increment = function(k, val) {
-    // If there are less than k elements in the stack
-    if (k > this.stack.length) {
-        this.stack = this.stack.map(ele => ele + val)
-    } else {
-        this.stack = this.stack.map((ele, idx) => {
-            if (idx < k){
-               return ele + val
-            } else return ele
-        }) 
-    }
-    console.log(this.stack)
+    const idx = Math.min(k, this.stack.length ) - 1;
+    this.inc[idx] += val;
 };
 
 /** 
