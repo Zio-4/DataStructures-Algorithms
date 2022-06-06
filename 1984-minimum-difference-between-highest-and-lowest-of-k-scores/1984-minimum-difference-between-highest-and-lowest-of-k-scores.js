804ba6c4d35620a569000948c4371e345cbf1a06
Philip Ziolkowski <80180474+Zio-4@users.noreply.github.com>
2022-06-06 14:15:13 -0700

    Time: 121 ms (30.26%), Space: 48.9 MB (7.89%) - LeetHub

---
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minimumDifference = function(nums, k) {
+    if (k === 1) return 0
+    
+    let minDiff = Infinity
+    
+    nums.sort((a, b) => a - b) 
+    
+    
+    for (let i = 0; i < nums.length - k + 1; i++) {
+        let sub = nums.slice(i, i+k)
+        
+        let diff = Math.abs(sub[0] - sub[sub.length - 1])
+        
+        minDiff = Math.min(minDiff, diff)
+    }
+    
+    return minDiff
+};
+
+// 1,4,7,9    k = 2
+
+// [1, 2, 3, 50, 99,100 ] k = 3
+
+// [10, 7, 1, 2 18] k = 3
