28d89a97d3743b68603864667bf350e950c02a07
Philip Ziolkowski <80180474+Zio-4@users.noreply.github.com>
2022-06-13 16:41:48 -0700

    Time: 103 ms (22.90%), Space: 43.4 MB (54.47%) - LeetHub

---
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const map = new Map()
+    let result = []
+    
+    for (let i = 0; i < nums1.length; i++) {
+        if (!map.has(nums1[i])) map.set(nums1[i], 1)
+    }
+    
+    for (let i = 0; i < nums2.length; i++) {
+        if (map.has(nums2[i]) && map.get(nums2[i]) !== 0) {
+            result.push(nums2[i])
+            map.set(nums2[i], 0)
+        }
+    }
+    return result
+};
+
+// 
+    
+//    [4,9,5]   [4,9,8,2,3,5] 
+
+// [4,5,9]  [2,3,4,5,8,9]
