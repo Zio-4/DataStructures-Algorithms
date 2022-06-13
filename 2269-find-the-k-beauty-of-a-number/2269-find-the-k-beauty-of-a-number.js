08c00b0bc2ced33bdce274b51c6ba797d2503860
Philip Ziolkowski <80180474+Zio-4@users.noreply.github.com>
2022-06-13 15:03:40 -0700

    Time: 89 ms (48.64%), Space: 42.4 MB (32.59%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.js
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.js
@@ -4,17 +4,15 @@
  * @return {number}
  */
 var divisorSubstrings = function(num, k) {
-    let strNum = num.toString()
-    
+    let s = num.toString()
     let kBeauty = 0
     
-    for (let i = 0; i < strNum.length - k + 1; i++) {
-        let subNum = strNum.slice(i, (i+k))
+    for (let i = 0; i < s.length - k + 1; i++) {
+        let sub = s.slice(i, i + k)
         
-        let parsedNum = parseInt(subNum)
-        
-        
-        if (num % parsedNum === 0) kBeauty++
+        let number = parseInt(sub)
+
+        if (num % number === 0) kBeauty++
     }
     
     return kBeauty
