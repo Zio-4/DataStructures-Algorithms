4898ae1648718f4e604efafc9653a5a523cbc86d
Philip Ziolkowski <80180474+Zio-4@users.noreply.github.com>
2022-06-16 18:58:41 -0700

    Time: 102 ms (21.19%), Space: 42.1 MB (56.07%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    if (!s) return true
+    if (!s && !t) return true
+    if (s && !t) return false
+    
+    let counter = 0
+    
+    let pointer = 0
+    for (let i = 0; i < t.length; i++) {
+        if (s[pointer] === t[i] ) {
+            counter++
+            pointer++
+        }
+        if (counter === s.length) break
+    }
+    
+    return counter === s.length
+};
+
+// TIme: 11 +
+
+// 'abbbbb' 'afsdbbbfsdb'
+
+// "axc"  "xacfscfdx"
