a02486d3031c3e1f749aa72c1a31767efa933c66
Philip Ziolkowski <80180474+Zio-4@users.noreply.github.com>
2022-08-01 11:57:34 -0700

    Time: 94 ms (54.92%), Space: 43.3 MB (94.12%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.js
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.js
@@ -10,59 +10,26 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-// var rightSideView = function(root) {
-//     if (!root) return []
-//     if (!root.left && !root.right) return [root.val]
-    
-//     let res = [root.val]
-    
-//     dfs(root)
-    
-//     return res
-    
-//     function dfs(node) {
-//         if (!node) return
-        
-//         if (node.right) {
-//             res.push(node.right.val)
-//             return dfs(node.right)
-//         } else if (node.left) {
-//             res.push(node.left.val)
-//             return dfs(node.left)
-//         }
-//     }
-// };
-
 var rightSideView = function(root) {
     if (!root) return []
     if (!root.left && !root.right) return [root.val]
     
-    let res = []
-    let q = [root]
-    let currLvl = []
+    const res = []
+    const q = [root]
     
     while(q.length) {
         const len = q.length
+        
         for (let i = 0; i < len; i++) {
             const node = q.shift()
             
             if (node.left) q.push(node.left)
             if (node.right) q.push(node.right)
             
-            currLvl.push(node.val)
+            if (i === len - 1) res.push(node.val)
         }
-        
-        res.push(currLvl.pop())
-        currLvl = []
     }
     
     return res
     
 };
-
-Approach:
-
-// DFS
-// Always check if there is a right side, if there is go down right
-// Otherwise go left
-// Repeat until at root
