1d35e77a41393ccdf5d76e086996e4cd339cb869
Philip Ziolkowski <80180474+Zio-4@users.noreply.github.com>
2022-08-01 11:08:38 -0700

    Time: 121 ms (49.92%), Space: 46.8 MB (89.11%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.js
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.js
@@ -21,7 +21,6 @@ var averageOfLevels = function(root) {
         let currLvlAvg = 0
         for (let i = 0; i < len; i++) {
             let node = q.shift()
-            console.log(node)
             if (node.left) q.push(node.left)
             if (node.right) q.push(node.right)
             
