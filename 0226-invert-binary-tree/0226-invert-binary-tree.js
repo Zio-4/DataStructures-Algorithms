f40f39f50c97b6e1703d45abec4dc7ddf59c9069
Philip Ziolkowski <80180474+Zio-4@users.noreply.github.com>
2022-10-18 19:37:33 -0700

    Time: 106 ms (26.02%), Space: 42.8 MB (6.00%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (!root) return null;
+    if (!root.left & !root.right) return root;
+    
+    [root.right, root.left] = [invertTree(root.left), invertTree(root.right)];
+    
+    return root;
+};
+
+// Edge cases:
+
+    // There are no nodes
+    // There is only the root
+    // For a node, only one side has a child node
+
+// Approach
+
+// DFS down the tree and at every swap the left and right
