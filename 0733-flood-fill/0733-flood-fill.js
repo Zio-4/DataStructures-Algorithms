ca7bbf22c8ea6a1b11c18190baca5706d047d2f5
Philip Ziolkowski <80180474+Zio-4@users.noreply.github.com>
2022-11-09 20:52:36 -0800

    Time: 107 ms (76.55%), Space: 44.3 MB (52.72%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, color) {
+    let y = sr
+    let x = sc
+    
+    const startColor = image[sr][sc]
+    
+    function dfs(row, col) {
+       //      top
+       // left        right
+       //      bottom
+        
+        if (row < 0 || col >= image[0].length || 
+            row >= image.length || 
+            col < 0 ||
+            image[row][col] !== startColor || 
+            image[row][col] === color) {
+            return
+        }
+        
+        image[row][col] = color
+      
+        dfs(row - 1, col)
+        dfs(row, col + 1)
+        dfs(row + 1, col)
+        dfs(row, col - 1)
+        
+    }
+    
+    dfs(y, x)
+    
+    return image
+};
+
+// DFS on the matrix
+
+// Conditions out of bounds in any direction || color is different || the color is already the new one
