523d570b65282d810328d6271c4fbd91d5141863
Philip Ziolkowski <80180474+Zio-4@users.noreply.github.com>
2022-11-18 16:46:04 -0800

    Time: 106 ms (99.99%), Space: 53.1 MB (37.52%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,56 +3,34 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    let res = []
-    
     nums.sort((a, b) => a - b)
     
-    for (let i = 0; i < nums.length; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let res = []
+    
+    for (let left = 0; left < nums.length; left++) {
+        if (left > 0 && nums[left] == nums[left - 1]) continue;
         
-        let a = nums[i]
+        let mid = left + 1
         
-        let left = i + 1
         let right = nums.length - 1
         
-        while (left < right) {
-            const sum = a + nums[left] + nums[right]
+        while (mid < right) {
+            let sum = nums[left] + nums[mid] + nums[right]
             
             if (sum > 0) {
                 right--
             } else if (sum < 0) {
-                left++
-            } else {
-                res.push([a,nums[left],nums[right]])
-                left++
-                while (nums[left] === nums[left - 1] && left < right) {
-                    left++
+                mid++
+            } else if (sum == 0) {
+                res.push([nums[left],nums[mid],nums[right]])
+                mid++
+                while (nums[mid] === nums[mid-1] && left < right) {
+                    mid++
                 }
             }
         }
     }
     
     return res
+
 };
-
-// Approach
-
-// Sort the input to avoid duplicate triplets
-
-// Iterate through the array and for each value, create a left and right pointer
-
-    // The left starts at i + 1 and the right at the end
-
-// In the loop, we need to do another loop - while
-
-    // We caculate the sum and if not equal to the target move the pointers
-
-    // If the sum is too big, we move the right pointer down
-
-    // If the sum is too small OR the next value is the same as the past, we move it forward
-
-    // If sum is found, add it to the result and
-        // move the left pointer up
-        // again check if the previous value is the same and that the left pointer is still before the right
-
-// return the result
