c39d8b2941757825ee3d95024588f6eb8f61fb96
Philip Ziolkowski <80180474+Zio-4@users.noreply.github.com>
2022-11-21 16:36:29 -0800

    Time: 60 ms (98.56%), Space: 44.6 MB (12.57%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (strs.length < 2) return strs[0]
+    
+    strs.sort((a,b) => a.length - b.length)
+    
+    let pre = strs[0].split('')
+    
+    for (let i = 1; i < strs.length; i++) {
+        let word = strs[i]
+        
+        // word can be ""
+        if (!word.length) return ""
+        
+        for (let j = 0; j < word.length; j++) {
+            if (j == 0 && pre[0] !== word[j]) {
+                return ""
+            }
+            
+            if (word[j] !== pre[j]) {
+                pre.splice(j)
+            }
+        }
+    }
+    
+    return pre.join('')
+};
+
+
+// APPROACH
+
+
+    // Take the first and use it as base for prefix
+        // split it into an array to easily remove end characters
+
+    // EDGE CASE: If the first letter does not match in any word, we can break the loop and return ""
+
+    // Compare every letter int he next word to the base prefix
+        // if a letter does not match, then from there we pop off of the base prefix array
+
