df6b837a8ae9c01ca069dae96a703be36235af5d
Philip Ziolkowski <80180474+Zio-4@users.noreply.github.com>
2022-11-21 16:43:36 -0800

    Time: 76 ms (86.41%), Space: 44.1 MB (19.69%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -2,32 +2,45 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-    if (strs.length < 2) return strs[0]
+// var longestCommonPrefix = function(strs) {
+//     if (strs.length < 2) return strs[0]
     
-    strs.sort((a,b) => a.length - b.length)
+//     strs.sort((a,b) => a.length - b.length)
     
-    let pre = strs[0].split('')
+//     let pre = strs[0].split('')
     
-    for (let i = 1; i < strs.length; i++) {
-        let word = strs[i]
+//     for (let i = 1; i < strs.length; i++) {
+//         let word = strs[i]
         
-        // word can be ""
-        if (!word.length) return ""
+//         // word can be ""
+//         if (!word.length) return ""
         
-        for (let j = 0; j < word.length; j++) {
-            if (j == 0 && pre[0] !== word[j]) {
-                return ""
-            }
+//         for (let j = 0; j < word.length; j++) {
+//             if (j == 0 && pre[0] !== word[j]) {
+//                 return ""
+//             }
             
-            if (word[j] !== pre[j]) {
-                pre.splice(j)
-            }
+//             if (word[j] !== pre[j]) {
+//                 pre.splice(j)
+//             }
+//         }
+//     }
+    
+//     return pre.join('')
+// };
+
+var longestCommonPrefix = function(strs) {
+    if(!strs.length) return '';
+    
+    for(let i = 0; i < strs[0].length; i++) {
+        for(let s of strs) {
+            if(s[i] !== strs[0][i]) return s.slice(0, i);
         }
     }
-    
-    return pre.join('')
+    return strs[0];
 };
+
+// 17 min
 
 
 // APPROACH
