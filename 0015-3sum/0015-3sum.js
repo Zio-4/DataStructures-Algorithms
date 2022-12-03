dcd6a46d9f57ce976077aba4d7dd7e25966115d6
Philip Ziolkowski <80180474+Zio-4@users.noreply.github.com>
2022-12-02 22:08:26 -0800

    Time: 234 ms (63.00%), Space: 52.5 MB (67.84%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,34 +3,41 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    nums.sort((a, b) => a - b)
+    nums.sort((a,b) => a - b)
     
     let res = []
     
-    for (let left = 0; left < nums.length; left++) {
-        if (left > 0 && nums[left] == nums[left - 1]) continue;
+    for (let anchor = 0; anchor < nums.length - 2; anchor++) {
+        if (anchor > 0 && nums[anchor] === nums[anchor - 1]) continue;
         
-        let mid = left + 1
-        
+        let left = anchor + 1
         let right = nums.length - 1
         
-        while (mid < right) {
-            let sum = nums[left] + nums[mid] + nums[right]
+        while (left < right) {
+            let sum = nums[anchor] + nums[left] + nums[right]
             
             if (sum > 0) {
                 right--
             } else if (sum < 0) {
-                mid++
-            } else if (sum == 0) {
-                res.push([nums[left],nums[mid],nums[right]])
-                mid++
-                while (nums[mid] === nums[mid-1] && left < right) {
-                    mid++
-                }
+                left++
+            } else {
+                // edge case for moving left up: previous is the same
+                // push in pair
+                res.push([nums[anchor],nums[left],nums[right]])
+                left++
+                while (left < right && nums[left] === nums[left - 1]) left++
             }
         }
     }
     
     return res
-
 };
+
+
+// sort to handle duplicates 
+
+// APPROACH:
+
+// 3 pointers: One anchor, and two pointers that move up and down.
+    // The anchor pointer will start at the front [left most]
+
